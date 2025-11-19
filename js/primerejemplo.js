1b988fed81495e4a06bf6827062c123c84a6c2a1
arnoldbgq <arnoldb.gq@gmail.com>
2025-11-19 13:04:56 -0500

    tarea

--- js/primerejemplo.js
+++ js/primerejemplo.js
@@ -3,6 +3,7 @@ alert("Hola Mundo, esto es un alert")
 
 function triste(){
     document.getElementById('carita').src='images/sad.png';
+    alert("Po que ta triste mano");
 }
 function feliz(){
     document.getElementById('carita').src='images/happy.png';

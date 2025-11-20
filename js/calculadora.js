c0a58f6fec34eee1f8ea209c765176d2f7d9525e
arnoldbgq <arnoldb.gq@gmail.com>
2025-11-20 16:57:30 -0500

    c

--- js/calculadora.js
+++ js/calculadora.js
@@ -1,3 +1,4 @@
+//el error estaba en el HTML se movio el script al footer, por un tema DOM ya que existe otro formulario en la misma pagina.
 document.getElementById("calcular").addEventListener("click", function(){
     let num1 = parseFloat(document.getElementById("numero1").value);
     let num2 = parseFloat(document.getElementById("numero2").value);

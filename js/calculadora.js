25d2c22a3bd1359c9184168ffc22575da12032d0
arnoldbgq <arnoldb.gq@gmail.com>
2025-11-20 13:55:34 -0500

    sds

---
+++ js/calculadora.js
@@ -0,0 +1,31 @@
+document.getElementById("calcular").addEventListener("click", function()){
+    let num1 = parseFloat(document.getElementById("numero1").value);
+    let num2 = parseFloat(document.getElementById("numero2").value);
+    let operacion = document.getElementById("operacion").value;
+    let resultado = document.getElementById("resultado");
+
+    if(isNaN(num1)||isNaN(num2)){
+        resultado.textContet = "Ingrese número Válidos";
+        return;
+    }
+function calcular(a,b,op){
+    switch (op) {
+        case "suma":
+            return a+b;
+            break;
+        case "resta":
+            return a-b;
+            break;
+        case "multiplicacion":
+            return a*b;
+            break;
+        case "division":
+            return a/b;
+            break;
+        default:
+            return "Operación Inválida"
+            
+    }
+}
+
+}
